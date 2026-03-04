995d924daed2f74e2be04920947b8b2e1683af17
Yaroslav <solomatin.yarik@mail.ru>
2026-03-04 11:29:48 +0300

    updated header

--- Views/components/header.php
+++ Views/components/header.php
@@ -1,6 +1,8 @@
 <header class="header">
-    <img src="/assets/images/logo.svg" alt="Logo" class="header__logo">
-    <p class="header__title">Галактический<br>
-        вестник
-    </p>
+    <a href="/" class="header__container header__link">
+        <img src="/assets/images/logo.svg" alt="Logo" class="header__logo">
+        <p class="header__title">Галактический<br>
+            вестник
+        </p>
+    </a>
 </header>
